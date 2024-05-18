76992df569f045ad6bd82d96579c496d3e657ba0
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2024-05-18 16:40:19 +0100

    create banner and why page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { FiChevronsDown, FiFacebook, FiInstagram, FiLinkedin } from 'react-icons/fi';
+import { FiChevronsDown } from 'react-icons/fi';
 import { Testemonials } from './components/testimonials';
 import { Header } from './components/header';
 import Footer from './components/footer';
 import WorkBox from './components/workbox';
-import { countries } from './services/countries';
 
 export default function Home() {
   // const [colorScheme, setColorScheme] = useState('light');
@@ -103,32 +100,70 @@ export default function Home() {
       <section className='limit-width why-container'>
         <h2>Porque nós escolher</h2>
 
-        <div>
-          <h3>Experiência e Expertise</h3>
-          <p>Nossa equipe possui ampla experiência e expertise em investimentos para garantir o melhor serviço aos nossos clientes.</p>
+        <div className='why-box'>
+          <div>
+            <h3>Experiência e Expertise</h3>
+            <p>Nossa equipe possui ampla experiência e expertise em investimentos para garantir o melhor serviço aos nossos clientes.
+              Contamos com profissionais qualificados que possuem profundo conhecimento do mercado financeiro e das diversas oportunidades de investimento.
+              Estamos sempre em busca de atualizações e aperfeiçoamentos para nos mantermos à frente das tendências e mudanças do mercado,
+              oferecendo estratégias personalizadas e eficazes.
+            </p>
+          </div>
+          <div>
+            <Image src={'/experiencia.webp'} alt='' width={600} height={400}/>
+          </div>
         </div>
-        <div>
-          <h3>Compromisso com o Sucesso</h3>
-          <p>Estamos comprometidos com o sucesso financeiro dos nossos clientes, priorizando sua satisfação e sucesso.</p>
+        <div className='why-box'>
+          <div>
+            <h3>Compromisso com o Sucesso</h3>
+            <p>Estamos comprometidos com o sucesso financeiro dos nossos clientes, priorizando sua satisfação e sucesso.
+              Entendemos profundamente os objetivos de cada cliente, criando planos de investimento alinhados com suas metas.
+              Trabalhamos incansavelmente para maximizar retornos e minimizar riscos, oferecendo suporte contínuo e ajustando
+              estratégias conforme necessário para proporcionar tranquilidade e confiança.
+            </p>
+          </div>
+          <div>
+            <Image src={'/sucesso.webp'} alt='' width={600} height={400} className='shadow-xl'/>
+          </div>
         </div>
-        <div>
-          <h3>Transparência e Confiança</h3>
-          <p>Valorizamos a transparência em todas as nossas interações, fornecendo informações claras e orientação honesta.</p>
+        <div className='why-box'>
+          <div>
+            <h3>Transparência e Confiança</h3>
+            <p>Valorizamos a transparência em todas as nossas interações, fornecendo informações claras e orientação honesta.
+              Explicamos detalhadamente nossas estratégias, custos e riscos associados a cada investimento.
+              Mantemos nossos clientes constantemente informados sobre o desempenho de seus portfólios e promovemos uma comunicação aberta,
+              garantindo que cada cliente se sinta seguro e confiante em suas decisões de investimento.
+            </p>
+          </div>
+          <div>
+            <Image src={'/informacoes.webp'} alt='' width={600} height={400}/>
+          </div>
         </div>
       </section>
+
       <section className='limit-width contact-container'>
         <h2>Contato</h2>
         <p>Pronto para transformar seus investimentos? Entre em contato hoje mesmo para uma consulta inicial gratuita.</p>
 
         <form action="">
-          <label htmlFor="">Nome: </label>
-          <input type="text" placeholder='Digite o seu nome: ' />
           
-          <label htmlFor="">E-mail: </label>
-          <input type="text" placeholder='Digite o seu e-mail: ' />
+          <div>
+            <label htmlFor="">Nome: </label>
+            <input type="text" placeholder='Digite o seu nome: ' />
+          </div>
+          
+          <div>
+            <label htmlFor="">E-mail: </label>
+            <input type="text" placeholder='Digite o seu e-mail: ' />
+          </div>
 
-          <label htmlFor="">Telefone: </label>
-          <input type="tel" placeholder='Digite o seu telefone: ' />
+          <div>
+            <label htmlFor="">Telefone: </label>
+            <input type="tel" placeholder='Digite o seu telefone: ' />
+          </div>
+
+
+
         </form>
       </section>
       <Footer />
