085f764bef0fe1b4c746b258a2285174c5997323
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2024-05-26 18:53:50 +0100

    create interactive with colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Header } from './components/header';
 import { Banner } from './components/banner';
 import { AboutUs } from './components/about';
@@ -14,10 +14,12 @@ import TechnoBanner from './components/technobanner';
 import { useTheme } from './context/themeContext';
 import { IColorsTheme } from '@/app/interface/IColorsTheme'
 import Loading from './components/loading';
+import { FiSettings } from 'react-icons/fi';
 
 
 export default function Home() {
   const { color, setColor } = useTheme();
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     const storedColor = localStorage.getItem("color");
@@ -67,16 +69,33 @@ export default function Home() {
       { !loading && (<Loading />)}
       { loading && (
         <>
-        <Header />
-        <TechnoBanner />
-        <Exchange />
-        <AboutUs />
-        <HowWorks />
-        <Testemonials />
-        <WhyChoose />
-        <Contact />
-        <Footer />
-        </>
+          <Header />
+          <button onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)} className={`z-50 fixed flex justify-center items-center right-0 top-[40%] ${isActive ? 'w-32' : 'w-12'} h-12 shadow-lg background-${color.substring(1)} hover:brightness-90 transition-all`}>
+            <FiSettings size={25} />
+          </button>
+          {isActive && <>
+            <button onClick={() => {setColor('#00A8DB')}} onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)} className={`z-50 fixed flex justify-center items-center right-0 top-[45%] w-32 h-12 shadow-lg bg-[#00A8DB] hover:brightness-90`}>
+              Ativos
+            </button>
+            <button onClick={() => {setColor('#820056')}}  onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)}  className={`z-50 fixed flex justify-center items-center right-0 top-[50%] w-32 h-12 shadow-lg bg-[#820056] hover:brightness-90`}>
+              Performance
+            </button>
+            <button onClick={() => {setColor('#00A584')}}  onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)}  className={`z-50 fixed flex justify-center items-center right-0 top-[55%] w-32 h-12 shadow-lg bg-[#00A584] hover:brightness-90`}>
+              Analise
+            </button>
+            <button onClick={() => {setColor('#FFA500')}}  onMouseEnter={() => setIsActive(true)} onMouseLeave={() => setIsActive(false)}  className={`z-50 fixed flex justify-center items-center right-0 top-[60%] w-32 h-12 shadow-lg bg bg-[#FFA500] hover:brightness-90`}>
+              Volatilidade
+            </button>
+          </>}
+          <TechnoBanner />
+          <Exchange />
+          <AboutUs />
+          <HowWorks />
+          <Testemonials />
+          <WhyChoose />
+          <Contact />
+          <Footer />
+          </>
         )}
     </main>
   );
